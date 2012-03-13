e2abc4671df9df881ff85aa5fbbca1e8d2fa76b7
bruno.bodson@dbz.be <bruno.bodson@dbz.be@548c8a6a-1593-4390-ebf8-37b36f95632b>
2012-03-13 08:32:01 +0000

    fixed password

--- trunk/solideagle/plugins/ad/ConnectionLdap.php
+++ trunk/solideagle/plugins/ad/ConnectionLdap.php
@@ -17,12 +17,10 @@ class ConnectionLDAP
         //ldap_start_tls();
         $this->conn = ldap_connect('ldaps://S1.solideagle.lok') or die("Could not connect to server");  
         // bind to the LDAP server specified above 
-//        ldap_set_option(NULL, LDAP_OPT_DEBUG_LEVEL, 7);
-//        ldap_set_option($this->conn, LDAP_OPT_PROTOCOL_VERSION, 3);
         $r = ldap_bind($this->conn, AD_USERNAME, AD_PASSWORD) or die("Could not bind to server");     
 
         ldap_set_option($this->conn, LDAP_OPT_PROTOCOL_VERSION, 3);
-     //   ldap_set_option($this->conn, LDAP_OPT_REFERRALS, 0);
+        ldap_set_option($this->conn, LDAP_OPT_REFERRALS, 0);
     }
     
     public function __destruct()
@@ -38,13 +36,7 @@ class ConnectionLDAP
 
         $userInfo["objectclass"] = "user";
 
-        $dn = 'CN=' . $userInfo["cn"] . ',OU=gebruikers,DC=solideagle,DC=lok';
-
-        //$userInfo["useraccountcontrol"] = $enabled? "512" : "66050";
-
-        var_dump($userInfo);
-
-
+        $userInfo["useraccountcontrol"] = $enabled? "66048" : "66050";
 
         if (ldap_add($this->conn, $dn, $userInfo))
         {
@@ -57,23 +49,18 @@ class ConnectionLDAP
         if ($this->conn == null)
             return false;
         
-        $dn = 'CN=' . $userInfo['cn'] . ',OU=gebruikers,DC=solideagle,DC=lok';
-        
         $userInfo['objectclass'] = "user";
         unset($userInfo["cn"]);
         
         $sr = ldap_search($this->conn, $dn, "(uid=" . $userInfo['uid'] . ")");
         $ent = ldap_get_entries($this->conn, $sr);
         
-        // Deactivate
         $ac = $ent[0]["useraccountcontrol"][0];
 
         $disable = ($ac |  2); // set all bits plus bit 1 (=dec2)
         $enable = ($ac & ~2); // set all bits minus bit 1 (=dec2)
  
         $userInfo["useraccountcontrol"] = $enabled? $enable : $disable;
-        
-        var_dump($userInfo);
         
         if (ldap_modify($this->conn, $dn, $userInfo))
         {
