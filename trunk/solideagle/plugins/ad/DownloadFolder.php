4a383eafa1014163ad7df23bd254b2f95d09aac6
bruno.bodson@dbz.be <bruno.bodson@dbz.be@548c8a6a-1593-4390-ebf8-37b36f95632b>
2012-03-20 16:00:15 +0000

    fixed downloadfolder script

--- trunk/solideagle/plugins/ad/DownloadFolder.php
+++ trunk/solideagle/plugins/ad/DownloadFolder.php
@@ -9,7 +9,7 @@ use Logging\Logger;
 
 class DownloadFolder
 {
-    public static function createDownloadFolder($server, $path, $username)
+    public static function createDownloadFolder($server, $path, $sharePath, $username)
     {
         $conn = new \Net_SSH2($server);
         if (!$conn->login(S1_ADMINISTRATOR, AD_PASSWORD))
@@ -20,8 +20,8 @@ class DownloadFolder
         
         $conn->write("cmd\n");
         
-        // create folder
-        $conn->write("mkdir " . $path . "\\" . $username ."\n");
+        // create download folder
+        $conn->write("mkdir " . $path . "\\" . $username . "\_downloads\n");
         
         // deny some special permissions
         $conn->write("icacls " . $path . "\\" . $username . " /q /deny " . AD_NETBIOS . "\\" . $username . ":(WDAC,WO,S)\n");
@@ -29,14 +29,18 @@ class DownloadFolder
         // allow modify
         $conn->write("icacls " . $path . "\\" . $username . " /q /grant " . AD_NETBIOS . "\\" . $username . ":M *S-1-5-32-544:F *S-1-5-18:F /inheritance:r /T /C\n");
 
-        $conn->write("icacls " . $path . "\\" . $username . " /grant *S-1-5-11:(CI)(RX)");
+        $conn->write("icacls " . $path . "\\" . $username . " /grant *S-1-5-11:(CI)(RX)\n");
         
+        $conn->write("setacl -ot file -actn ace -ace \"n:" . AD_NETBIOS . "\\Domain Users;s:n;p:read;i:so,sc\" -on " . $path . "\\" . $username . "\\_downloads\n");
 
-        while($data = $conn->_get_channel_packet(NET_SSH2_CHANNEL_SHELL))
-{
-	echo $data;
-}
+        // make link
+        $conn->write("mklink /j " . $sharePath . "\\" . $username . ' ' . $path . "\\" . $username . "\\_downloads\n");
         
+//        while($data = $conn->_get_channel_packet(NET_SSH2_CHANNEL_SHELL))
+//{
+//	echo $data;
+//}
+
         $conn->write("exit\nexit\n");
         $conn->_close_channel(NET_SSH2_CHANNEL_SHELL); 
         $conn->disconnect();
