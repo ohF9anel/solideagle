bbed4a845ea8ac2c434022c29b3def95c644ff71
bruno.bodson@dbz.be <bruno.bodson@dbz.be@548c8a6a-1593-4390-ebf8-37b36f95632b>
2012-05-02 11:36:25 +0000

    gapps set photo TEST

--- solideagle/solideagle/plugins/ga/manageuser.php
+++ solideagle/solideagle/plugins/ga/manageuser.php
@@ -216,6 +216,46 @@ class manageuser
         
         return new StatusReport(!$errorHandler->hasErrors(), $errorHandler->toString());
     }
+    
+    public function downloadTempFile($url, $path, $filename)
+    {
+        ob_start();
+        passthru('wget ' . $path . ' 2>&1');
+        $out = ob_get_contents();
+        ob_end_clean();
+
+        if (preg_match('/saving to:.{4}([a-z0-9\.-_]*)/i', $out, $match)) {
+            return $match;
+        } else {
+            return "no match";
+        }
+
+//        $descriptorspec = array(
+//                        0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+//                        1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+//                        2 => array("pipe", "a")   // stderr is a file to write to
+//        );
+//        
+//        $cmd = "wget " . $url . " " . $path . " -O " . $filename;
+//
+//        $proc_ls = proc_open($cmd, $descriptorspec, $pipes);
+//
+//        while(true) 
+//        {   
+//            if(($buffer = fgets($pipes[1])) === false)
+//                break;
+//
+//            echo $buffer;
+//            flush();
+//        }
+//
+//        foreach ($pipes as $pipe)
+//            fclose($pipe);
+//
+//        proc_close($proc_ls);
+//        
+//        return $path . $filename;
+    }
             
 }
 
