75dd0d5cefb8520cad2aa7f8f7961287fd2fbaf1
borremansg@dbz.be <borremansg@dbz.be@548c8a6a-1593-4390-ebf8-37b36f95632b>
2012-08-25 17:56:38 +0000

    doc

--- trunk/solideagle/solideagle/InstallationPrerequisiteScripts/PearIncludePathTest.php
+++ trunk/solideagle/solideagle/InstallationPrerequisiteScripts/PearIncludePathTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/** 
+ * 
+ * PearIncludePathTest.php
+ * 
+ * @author Machiel Sleeuwaert <Machiel.Sleeuwaert@dbz.be>
+ * 
+ */
 require_once 'System.php'; //include path is not set or wrong
 
 require_once 'Log.php'; //pear Log is not installed
