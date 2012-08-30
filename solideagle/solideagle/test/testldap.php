b87dc2d6e242db693bad890d9713150425f5d6ae
borremansg@dbz.be <borremansg@dbz.be@548c8a6a-1593-4390-ebf8-37b36f95632b>
2012-08-30 11:34:01 +0000

    new basic ldap test file

--- solideagle/solideagle/test/testldap.php
+++ solideagle/solideagle/test/testldap.php
@@ -15,29 +15,29 @@ if ($ds) {
     echo "Binding ..."; 
     $r=ldap_bind($ds);     // this is an "anonymous" bind, typically
                            // read-only access
-    echo "Bind result is " . $r . "<br />";
+    echo "Bind result is " . $r . "<br />\n";
 
     echo "Searching for (sn=S*) ...";
     // Search surname entry
     $sr=ldap_search($ds, "o=My Company, c=US", "sn=S*");  
-    echo "Search result is " . $sr . "<br />";
+    echo "Search result is " . $sr . "<br />\n";
 
     echo "Number of entries returned is " . ldap_count_entries($ds, $sr) . "<br />";
 
-    echo "Getting entries ...<p>";
+    echo "Getting entries ...<p>\n";
     $info = ldap_get_entries($ds, $sr);
-    echo "Data for " . $info["count"] . " items returned:<p>";
+    echo "Data for " . $info["count"] . " items returned:<p>\n";
 
     for ($i=0; $i<$info["count"]; $i++) {
-        echo "dn is: " . $info[$i]["dn"] . "<br />";
-        echo "first cn entry is: " . $info[$i]["cn"][0] . "<br />";
-        echo "first email entry is: " . $info[$i]["mail"][0] . "<br /><hr />";
+        echo "dn is: " . $info[$i]["dn"] . "<br />\n";
+        echo "first cn entry is: " . $info[$i]["cn"][0] . "<br />\n";
+        echo "first email entry is: " . $info[$i]["mail"][0] . "<br /><hr />\n";
     }
 
-    echo "Closing connection";
+    echo "Closing connection\n";
     ldap_close($ds);
 
 } else {
-    echo "<h4>Unable to connect to LDAP server</h4>";
+    echo "\n<h4>Unable to connect to LDAP server</h4>";
 }
 ?>
